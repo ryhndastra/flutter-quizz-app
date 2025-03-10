29f4d44639c86cf2e8db85a97a8110fc192e3803
ryhndastra <ryhndastra@gmail.com>
2025-03-10 20:02:09 +0700

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 94, 2, 110),
+        body: Center(),
+      ),
+    ),
+  );
+}
