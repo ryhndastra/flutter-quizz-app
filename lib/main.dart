54bc659497d96f200782f6a973a16a6a01051320
ryhndastra <ryhndastra@gmail.com>
2025-03-10 20:59:48 +0700

    adding gradient color for the background

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:quizapp/start_quiz.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 94, 2, 110),
-        body: Center(),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartQuiz(),
+        ),
       ),
     ),
   );
